41985c9b96ac3e866c2cc7540e68ffb1ec2f2fcd
NageswaraRao <pythontrainernag@gmail.com>
2019-03-20 09:55:42 +0530

    20/03/2019

---
+++ arth.py
@@ -0,0 +1,13 @@
+"""This module/script is used to perform all arthematic operations"""
+
+def addi(first_var, sec_var):
+    """ This function is used perform the addition of two numbers"""
+    return first_var+sec_var
+
+def subs(first_var, sec_var):
+    """ This function is used perform the substraction of two numbers"""
+    return first_var-sec_var
+
+def mult(first_var, sec_var):
+    """ This function is used perform the multiplication of two numbers"""
+    return first_var*sec_var
---
+++ read.py
@@ -0,0 +1,15 @@
+#import arth
+"""
+from arth import addi, subs
+
+print addi(10, 15)
+print subs(15, 10)
+"""
+
+import arth as ar
+print ar.addi(10, 15)
+print ar.mult(10, 15)
+
+from arth import addi as ad
+print ad(10, 15)
+print dir(ar)
